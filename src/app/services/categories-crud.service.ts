3e7dd855b62537836cc5b49c568984715158d155
Wesley Aquiles <wesley419@gmail.com>
2022-05-19 20:23:35 -0300

    add environment

--- src/app/services/categories-crud.service.ts
+++ src/app/services/categories-crud.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Categories } from '../categories/Icategories';
 import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesCrudService {
 
-  private readonly API = 'http://localhost:3333/v1/categories'
+  private readonly API = `${environment.API}categories`
 
   constructor(private http: HttpClient) { }
 
